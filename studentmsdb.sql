1a76230b98e6dd1f3ad0a3b6ef52fd233ab3d5f5
Khilan Mangukiya <129717666+khilan4100@users.noreply.github.com>
2024-08-15 21:33:12 +0530

    create school website

---
+++ studentmsdb.sql
@@ -0,0 +1,254 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Aug 10, 2024 at 05:16 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `studentmsdb`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbladmin`
+--
+
+CREATE TABLE `tbladmin` (
+  `ID` int(10) NOT NULL,
+  `AdminName` varchar(120) DEFAULT NULL,
+  `UserName` varchar(120) DEFAULT NULL,
+  `MobileNumber` bigint(10) DEFAULT NULL,
+  `Email` varchar(200) DEFAULT NULL,
+  `Password` varchar(200) DEFAULT NULL,
+  `AdminRegdate` timestamp NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `tbladmin`
+--
+
+INSERT INTO `tbladmin` (`ID`, `AdminName`, `UserName`, `MobileNumber`, `Email`, `Password`, `AdminRegdate`) VALUES
+(1, 'Admin', 'admin', 8979555558, 'admin@gmail.com', '81dc9bdb52d04dc20036dbd8313ed055', '2024-08-08 14:03:44'),
+(2, 'Smit M', 'admin-smit', 9265281004, 'smiyani@bugrifix.com', '81dc9bdb52d04dc20036dbd8313ed055', '2024-08-10 14:30:20'),
+(5, 'admin-principal', 'principal', 9999999999, 'contact@sskvidhyalya.com', '81dc9bdb52d04dc20036dbd8313ed055', '2024-08-10 14:31:27');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tblclass`
+--
+
+CREATE TABLE `tblclass` (
+  `ID` int(5) NOT NULL,
+  `ClassName` varchar(50) DEFAULT NULL,
+  `Section` varchar(20) DEFAULT NULL,
+  `CreationDate` timestamp NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `tblclass`
+--
+
+INSERT INTO `tblclass` (`ID`, `ClassName`, `Section`, `CreationDate`) VALUES
+(1, '10', 'A', '2024-07-13 10:42:14'),
+(2, '10', 'B', '2024-07-13 10:42:35'),
+(3, '10', 'C', '2024-07-13 10:42:41'),
+(4, '11', 'A', '2024-07-13 10:42:47'),
+(5, '11', 'B', '2024-07-13 10:42:52'),
+(6, '11', 'C', '2024-07-13 10:42:57'),
+(7, '11', 'D', '2024-07-13 10:43:04'),
+(8, '12', 'A', '2024-07-13 10:43:10'),
+(9, '12', 'C', '2024-07-13 10:43:15');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tblnotice`
+--
+
+CREATE TABLE `tblnotice` (
+  `ID` int(5) NOT NULL,
+  `NoticeTitle` mediumtext DEFAULT NULL,
+  `ClassId` int(10) DEFAULT NULL,
+  `NoticeMsg` mediumtext DEFAULT NULL,
+  `CreationDate` timestamp NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `tblnotice`
+--
+
+INSERT INTO `tblnotice` (`ID`, `NoticeTitle`, `ClassId`, `NoticeMsg`, `CreationDate`) VALUES
+(2, 'Marks of Unit Test.', 3, 'Meet your class teacher for seeing copies of unit test', '2024-07-19 06:35:58'),
+(3, 'Marks of Unit Test.', 2, 'Meet your class teacher for seeing copies of unit test', '2024-07-19 06:35:58'),
+(4, 'Test', 3, 'This is for testing.', '2024-07-02 18:17:03'),
+(5, 'Test Notice', 8, 'This is for Testing.', '2024-07-02 19:03:43');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tblpage`
+--
+
+CREATE TABLE `tblpage` (
+  `ID` int(10) NOT NULL,
+  `PageType` varchar(200) DEFAULT NULL,
+  `PageTitle` mediumtext DEFAULT NULL,
+  `PageDescription` mediumtext DEFAULT NULL,
+  `Email` varchar(200) DEFAULT NULL,
+  `MobileNumber` bigint(10) DEFAULT NULL,
+  `UpdationDate` date DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `tblpage`
+--
+
+INSERT INTO `tblpage` (`ID`, `PageType`, `PageTitle`, `PageDescription`, `Email`, `MobileNumber`, `UpdationDate`) VALUES
+(1, 'aboutus', 'About Us', '<div style=\"text-align: start;\"><font color=\"#7b8898\" face=\"Mercury SSm A, Mercury SSm B, Georgia, Times, Times New Roman, Microsoft YaHei New, Microsoft Yahei, ????, ??, SimSun, STXihei, ????, serif\"><span style=\"font-size: 26px;\">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</span></font><br></div>', NULL, NULL, NULL),
+(2, 'contactus', 'Contact Us', '890,Sector 62, Gyan Sarovar, GAIL Noida(Delhi/NCR)', 'infodata@gmail.com', 7896541236, NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tblpublicnotice`
+--
+
+CREATE TABLE `tblpublicnotice` (
+  `ID` int(5) NOT NULL,
+  `NoticeTitle` varchar(200) DEFAULT NULL,
+  `NoticeMessage` mediumtext DEFAULT NULL,
+  `CreationDate` timestamp NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `tblpublicnotice`
+--
+
+INSERT INTO `tblpublicnotice` (`ID`, `NoticeTitle`, `NoticeMessage`, `CreationDate`) VALUES
+(1, 'School will re-open', 'Consult your class teacher.', '2024-07-20 09:11:57'),
+(2, 'Test Public Notice', 'This is for Testing\r\n', '2024-07-02 19:04:10');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tblstudent`
+--
+
+CREATE TABLE `tblstudent` (
+  `ID` int(10) NOT NULL,
+  `StudentName` varchar(200) DEFAULT NULL,
+  `StudentEmail` varchar(200) DEFAULT NULL,
+  `StudentClass` varchar(100) DEFAULT NULL,
+  `Gender` varchar(50) DEFAULT NULL,
+  `DOB` date DEFAULT NULL,
+  `StuID` varchar(200) DEFAULT NULL,
+  `FatherName` mediumtext DEFAULT NULL,
+  `MotherName` mediumtext DEFAULT NULL,
+  `ContactNumber` bigint(10) DEFAULT NULL,
+  `AltenateNumber` bigint(10) DEFAULT NULL,
+  `Address` mediumtext DEFAULT NULL,
+  `UserName` varchar(200) DEFAULT NULL,
+  `Password` varchar(200) DEFAULT NULL,
+  `Image` varchar(200) DEFAULT NULL,
+  `DateofAdmission` timestamp NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `tbladmin`
+--
+ALTER TABLE `tbladmin`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- Indexes for table `tblclass`
+--
+ALTER TABLE `tblclass`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- Indexes for table `tblnotice`
+--
+ALTER TABLE `tblnotice`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- Indexes for table `tblpage`
+--
+ALTER TABLE `tblpage`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- Indexes for table `tblpublicnotice`
+--
+ALTER TABLE `tblpublicnotice`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- Indexes for table `tblstudent`
+--
+ALTER TABLE `tblstudent`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `tbladmin`
+--
+ALTER TABLE `tbladmin`
+  MODIFY `ID` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `tblclass`
+--
+ALTER TABLE `tblclass`
+  MODIFY `ID` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+
+--
+-- AUTO_INCREMENT for table `tblnotice`
+--
+ALTER TABLE `tblnotice`
+  MODIFY `ID` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `tblpage`
+--
+ALTER TABLE `tblpage`
+  MODIFY `ID` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `tblpublicnotice`
+--
+ALTER TABLE `tblpublicnotice`
+  MODIFY `ID` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `tblstudent`
+--
+ALTER TABLE `tblstudent`
+  MODIFY `ID` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
